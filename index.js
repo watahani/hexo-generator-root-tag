4d5e617c36700fc6412386d98596cd58d5aa7469
Wataru Haniyama <haniyama.wataru+work@gmail.com>
2019-07-06 08:54:46 +0900

    fix root_tag_generator option

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 
 hexo.config.root_tag_generator = Object.assign({
   per_page: hexo.config.per_page == null ? 10 : hexo.config.per_page
-}, hexo.config.tag_generator);
+}, hexo.config.root_tag_generator);
 
 hexo.extend.generator.register('root_tag', require('./lib/generator'));
 hexo.extend.helper.register('list_tag_tree', require('./lib/helper').listRootTagHelper);
