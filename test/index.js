8105b87adf652154cf77d6bbd15443db813d4e9a
Wataru Haniyama <haniyama.wataru+job@gmail.com>
2021-05-09 22:43:40 +0900

    update hexo 5.4.0

--- test/index.js
+++ test/index.js
@@ -1,15 +1,15 @@
 'use strict';
 
 var should = require('chai').should(); // eslint-disable-line
-var Hexo = require('hexo');
+const Hexo = require('hexo');
 const defaultOption = require('../lib/helper').defaultOption;
 
-describe('Root Tag generator', function() {
-  var hexo = new Hexo(__dirname, {silent: true});
+describe('Root Tag generator', () => {
+  const hexo = new Hexo(__dirname, {silent: true});
   hexo.init();
-  var Post = hexo.model('Post');
-  var generator = require('../lib/generator').bind(hexo);
-  var posts,
+  const Post = hexo.model('Post');
+  const generator = require('../lib/generator').bind(hexo);
+  let posts,
     locals;
 
   // Default config
@@ -18,34 +18,34 @@ describe('Root Tag generator', function() {
   };
   hexo.config.root_tag_generator = defaultOption;
 
-  before(function() {
+  before(() => {
     return Post.insert([
       {source: 'foo', slug: 'foo', date: 1e8},
       {source: 'bar', slug: 'bar', date: 1e8 + 1},
       {source: 'baz', slug: 'baz', date: 1e8 - 1}
-    ]).then(function(data) {
+    ]).then(data => {
       posts = data;
       return posts[0].setTags(['foo', 'foo1', 'foo2']);
-    }).then(function() {
+    }).then(() => {
       return posts[1].setTags(['bar', 'bar1', 'bar2']);
-    }).then(function() {
+    }).then(() => {
       return posts[2].setTags(['piyo', 'bar', 'foo']);
-    }).then(function() {
+    }).then(() => {
       locals = hexo.locals.toObject();
     });
   });
 
-  describe('set root tags', function() {
-    it('set sub tag limit 2', function() {
+  describe('set root tags', () => {
+    it('set sub tag limit 2', () => {
       hexo.config.root_tag_generator = {
         sub_tag_limit: 2,
         root_tag_names: ['foo']
       };
-      var result = generator(locals);
+      const result = generator(locals);
       // 7 tags ->
       result.length.should.eql(2);
 
-      for (var i = 0, len = result.length; i < len; i++) {
+      for (let i = 0, len = result.length; i < len; i++) {
         result[i].layout.should.eql(['tag', 'archive', 'index']);
       }
 
@@ -57,16 +57,16 @@ describe('Root Tag generator', function() {
       hexo.config.root_tag_generator = defaultOption;
     });
 
-    it('set multi root_tag_names', function() {
+    it('set multi root_tag_names', () => {
       hexo.config.root_tag_generator = {
         sub_tag_limit: 2,
         root_tag_names: ['foo', 'bar']
       };
-      var result = generator(locals);
+      const result = generator(locals);
       // 7 tags ->
       result.length.should.eql(4);
 
-      for (var i = 0, len = result.length; i < len; i++) {
+      for (let i = 0, len = result.length; i < len; i++) {
         result[i].layout.should.eql(['tag', 'archive', 'index']);
       }
 
@@ -79,13 +79,13 @@ describe('Root Tag generator', function() {
       hexo.config.root_tag_generator = defaultOption;
     });
 
-    it('no root_tag_names option', function() {
+    it('no root_tag_names option', () => {
       hexo.config.root_tag_generator = {
         sub_tag_limit: 10,
         root_tag_names: []
       };
 
-      var result = generator(locals);
+      const result = generator(locals);
 
       result.length.should.eql(0);
 
@@ -93,27 +93,27 @@ describe('Root Tag generator', function() {
       hexo.config.tag_generator.per_page = defaultOption;
     });
 
-    it('set root_tag_names "all"', function() {
+    it('set root_tag_names "all"', () => {
       hexo.config.root_tag_generator = {
         sub_tag_limit: 10,
         root_tag_names: 'all'
       };
 
-      var result = generator(locals);
+      const result = generator(locals);
 
-      result.filter((item) => {
+      result.filter(item => {
         return item.path.match('tags/foo/');
       }).length.should.eql(4);
 
-      result.filter((item) => {
+      result.filter(item => {
         return item.path.match('tags/bar/');
       }).length.should.eql(4);
 
-      result.filter((item) => {
+      result.filter(item => {
         return item.path.match('tags/piyo/');
       }).length.should.eql(2);
 
-      result.filter((item) => {
+      result.filter(item => {
         return item.path.match('tags/foo1/');
       }).length.should.eql(2);
 
